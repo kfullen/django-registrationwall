528e1393792add877ede9aad50bafb4bc611df73
Mark Wilkening <wilkening.mark@gmail.com>
2020-11-29 12:44:40 -0500

    library version updates

--- demo/demo/articles/models.py
+++ demo/demo/articles/models.py
@@ -1,9 +1,9 @@
 from __future__ import unicode_literals
 
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.db import models
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 
